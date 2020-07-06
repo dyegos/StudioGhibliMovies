7a5023ec8efb3c8e48e0e48579bcb3cf21e54f46
Dyego Silva <dyego.j.silva@gmail.com>
2020-07-06 10:23:58 +0200

    Made AppDelegate a public class, fixed code sign

--- StudioGhibliMovies/StudioGhibliMovies/AppDelegate.swift
+++ StudioGhibliMovies/StudioGhibliMovies/AppDelegate.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+public class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    public var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         let window = UIWindow()
         window.rootViewController = InitializationViewController()
